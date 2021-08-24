580221e934f3e4e83122a77dff6bf1825941b08c
gaokakao <giedriuskk@gmail.com>
2021-08-24 18:11:33 +0000

    Pradedu darbus

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update
