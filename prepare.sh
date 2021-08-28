97b05cdf6d1e226f861ee8acd505968720644b2e
giedrius.kalytis <giedrius.kalytis@adv.dev>
2021-08-28 12:10:35 +0000

    pridejau reboot playbook ir padariau kad išinstaliuotų ir pilnai ištrintų apparmor

--- prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-apt-get update
