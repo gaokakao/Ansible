14d49afcd8f1c5070eee6c569163fd57739df69a
giedrius.kalytis <giedrius.kalytis@adv.bet>
2021-09-16 18:11:33 +0300

    pirmas task Done

---
+++ roles/mc/files/script.sh
@@ -0,0 +1,5 @@
+grep -l bash\.bashrc /etc/profile || (echo
+    echo 'test "$PS1" && test "$BASH" && . /etc/bash.bashrc') |
+    tee -a /etc/profile
+echo 'type -p -a mc >/dev/null &&
+    alias mc=". /usr/share/mc/bin/mc-wrapper.sh"' | tee -a /etc/bash.bashrc
