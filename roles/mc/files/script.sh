a6c796a670f8385c5bb82930143c5dab355129d8
gaokakao <giedriuskk@gmail.com>
2022-02-17 08:50:28 +0000

    transfer

--- roles/mc/files/script.sh
+++
@@ -1,5 +0,0 @@
-grep -l bash\.bashrc /etc/profile || (echo
-    echo 'test "$PS1" && test "$BASH" && . /etc/bash.bashrc') |
-    tee -a /etc/profile
-echo 'type -p -a mc >/dev/null &&
-    alias mc=". /usr/share/mc/bin/mc-wrapper.sh"' | tee -a /etc/bash.bashrc
